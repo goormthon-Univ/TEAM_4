b0d7bc69e66c6c09af646e0f830bb2402b59ce95
minwoohong <hsoapdc@gmail.com>
2023-11-13 01:41:57 +0900

    초기세팅

---
+++ floatcloud/src/Pages/Account/CreateAcc.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+// height는 추후 수정 필요 시 수정
+const Container = styled.div`
+  width: 100%;
+  height: 1024px;
+  position: fixed;
+`;
+
+const CreateAcc = () => {};
+export default CreateAcc;
