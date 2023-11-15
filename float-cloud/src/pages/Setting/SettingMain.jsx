94cd283804272091048e4af8103e985158b890e4
yunyun827 <imyure@gmail.com>
2023-11-15 17:14:43 +0900

    설정 페이지 버튼 및 텍스트 태그 개발

--- float-cloud/src/pages/Setting/SettingMain.jsx
+++ float-cloud/src/pages/Setting/SettingMain.jsx
@@ -17,6 +17,49 @@ const Header = styled.div`
   align-items: center;
 `;
 
+const BackgroundContainer = styled.div`
+
+  margin: 11.69rem 9.37rem 15rem 9.37rem;
+  background-color: #2864FF33;
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SectionTitle = styled.div`
+font-size: 1.875rem;
+  textAlign:left;
+  font-style: normal;
+  font-weight: 700;
+  display: flex; 
+  align-items: center;
+`;
+
+const StyledButton = styled.button`
+  color: var(--White, #FAFAFA);
+  font-family: SUIT;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 150%;  
+  border-radius: 3.125rem;
+  background: var(--Skyblue-70, rgba(0, 148, 255, 0.70));
+  border: none;
+  display: inline-flex;
+  padding: 0.625rem 1.25rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+`;
+
+const userState = styled.div`
+  width: 100%;
+  height: 1024px;
+  position: fixed;
+`;
+
+
 const EngLogo = styled.div``;
 
 const HeaderBtn = styled.div`
@@ -61,6 +104,18 @@ const SettingMain = () => {
           </GoSetting>
         </HeaderBtn>
       </Header>
+      <BackgroundContainer>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '90%', marginTop: '1.81rem' }}>
+          <SectionTitle>프로필</SectionTitle>
+          <StyledButton>변경된 프로필 저장하기</StyledButton>
+        </div>
+        <userState>
+          
+        
+        
+        </userState>
+
+      </BackgroundContainer>
     </Container>
   );
 };
